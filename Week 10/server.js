dbf58d4346cda1c5b73c75182305e28175b6aaac
xiaosheng <287915338@qq.com>
2021-01-06 20:57:57 +0800

    排版- 1 2

---
+++ Week 10/server.js
@@ -0,0 +1,37 @@
+const http = require('http')
+
+http.createServer((request, response) => {
+  console.log('require reslove')
+  console.log(request.headers)
+  response.setHeader('X-Foo', 'bar')
+  // response.setHeader('Content-Type', 'text/html')
+  response.writeHead(200, { 'Content-Type': 'text/html' })
+  response.end(
+    `<html author=tianzun>
+<head>
+  <title>Toy Browser</title>
+  <style>
+    body div #myid {
+      width: 100px;
+      height: 100px;
+      background-color: #ff0055;
+    }
+    body div img {
+      width: 30px;
+      height: 30px;
+      background-color: #ff1111;
+    }
+  </style>
+</head>
+<body>
+  <div>
+    <img id='myid' />
+    <img />
+  </div>
+</body>
+</html>
+`
+  )
+}).listen(8088)
+
+console.log('server started')
